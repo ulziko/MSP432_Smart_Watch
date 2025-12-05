e1c3974c8d34094cfee910556a34ad2daca8bbc5
ulziko <ulziihishig247@gmail.com>
2025-12-05 13:49:36 +0100

    main timer

--- main.c
+++ main.c
@@ -6,116 +6,132 @@
 #include "LcdDriver/Crystalfontz128x128_ST7735.h"
 #include "LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
+#include <time.h>
 
 /* Graphic library context */
 Graphics_Context g_sContext;
 
-/* Variable for storing lux value returned from OPT3001 */
-float lux;
+typedef struct {
+    int hour;
+    int minute;
+    int second;
+    char time_string[20];
+} TimeStruct;
 
-void _lightSensorInit()
-{
-    /* Initialize I2C communication */
-    Init_I2C_GPIO();
-    I2C_init();
+typedef struct{
+    int day;
+    int month;
+    int year;
+    char date_string[20];
+} DateStruct;
 
-    /* Initialize OPT3001 digital ambient light sensor */
-    OPT3001_init();
+TimeStruct ClockTime;
+DateStruct ClockDate;
 
-    __delay_cycles(100000);
 
-}
+
+/* Timer_A configuration for 1 second interrupt (ACLK = 32768 Hz) */
+const Timer_A_UpModeConfig upConfig = {
+    TIMER_A_CLOCKSOURCE_ACLK,
+    TIMER_A_CLOCKSOURCE_DIVIDER_1,
+    32767,                    // 1 second
+    TIMER_A_TAIE_INTERRUPT_DISABLE,
+    TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE,
+    TIMER_A_DO_CLEAR
+};
 
 void _graphicsInit()
 {
-    /* Initializes display */
     Crystalfontz128x128_Init();
-
-    /* Set default screen orientation */
     Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
 
-    /* Initializes graphics context */
-    Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128,
-                         &g_sCrystalfontz128x128_funcs);
-    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_RED);
-    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
-    GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+    Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128, &g_sCrystalfontz128x128_funcs);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE_SMOKE);
+    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_SLATE_BLUE);
+    GrContextFontSet(&g_sContext, &g_sFontCmsc24);
     Graphics_clearDisplay(&g_sContext);
-
 }
 
 void _hwInit()
 {
-    /* Halting WDT and disabling master interrupts */
     WDT_A_holdTimer();
     Interrupt_disableMaster();
 
-    /* Set the core voltage level to VCORE1 */
     PCM_setCoreVoltageLevel(PCM_VCORE1);
-
-    /* Set 2 flash wait states for Flash bank 0 and 1*/
     FlashCtl_setWaitState(FLASH_BANK0, 2);
     FlashCtl_setWaitState(FLASH_BANK1, 2);
 
-    /* Initializes Clock System */
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
-    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    CS_initClockSignal(CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
     CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
-    _graphicsInit();
-    _lightSensorInit();
+    Timer_A_configureUpMode(TIMER_A0_BASE, &upConfig);
+    Interrupt_enableInterrupt(INT_TA0_0);
+    Interrupt_enableMaster();
+
+    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
 }
 
-/*
- * Main function
- */
-
-void _graphicsDisplay_set_red()
+void syncTime()
 {
-    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_RED);
+    time_t current_time;
+    current_time = time(NULL);
+    struct tm *t = localtime(&current_time);
+    time ( &current_time );
+    ClockTime.hour = t->tm_hour;
+    ClockTime.minute = t->tm_min;
+    ClockTime.second = t->tm_sec;
+
+    ClockDate.day   = t->tm_mday;
+    ClockDate.month = t->tm_mon + 1;
+    ClockDate.year  = t->tm_year + 1900;
 }
 
-void _graphicsDisplay_set_blue()
+void DrawTime()
 {
-    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLUE);
+    sprintf(ClockTime.time_string, "%02d:%02d:%02d", ClockTime.hour, ClockTime.minute, ClockTime.second);
+    GrContextFontSet(&g_sContext, &g_sFontCmsc34);
+    Graphics_drawStringCentered(&g_sContext, (int8_t*)ClockTime.time_string, -1, 64, 60, OPAQUE_TEXT);
+}
+
+void DrawDate()
+{
+    sprintf(ClockDate.date_string, "%02d/%02d/%04d", ClockDate.day, ClockDate.month, ClockDate.year);
+    GrContextFontSet(&g_sContext, &g_sFontCmsc12);
+    Graphics_drawStringCentered(&g_sContext, (int8_t*)ClockDate.date_string, -1, 64, 110, OPAQUE_TEXT);
 }
 
 int main(void)
 {
+    _graphicsInit();
+    syncTime();
 
     _hwInit();
-
-    Graphics_drawStringCentered(&g_sContext, (int8_t *) "Light Sensor:",
-    AUTO_STRING_LENGTH,
-                                64, 30,
-                                OPAQUE_TEXT);
+    DrawTime();
+    DrawDate();
 
     while (1)
-    {
-
-        /* Obtain lux value from OPT3001 */
-        lux = OPT3001_getLux();
-        if (lux <5000){
-            _graphicsDisplay_set_blue();
-        }
-        else {
-            _graphicsDisplay_set_red();
-        }
-        char string[20];
-//
-        sprintf(string, "%f", lux);
-        Graphics_drawStringCentered(&g_sContext, (int8_t *) string, 6, 48, 70,
-        OPAQUE_TEXT);
-//
-        sprintf(string, "lux");
-        Graphics_drawStringCentered(&g_sContext, (int8_t *) string, 3, 86, 70,
-        OPAQUE_TEXT);
-        // drawing circle
-        Graphics_drawCircle(&g_sContext, 33, 64, 25);
-        Graphics_fillCircle(&g_sContext, 95, 64, 25);
-
-    }
+        PCM_gotoLPM0();
 }
 
+void TA0_0_IRQHandler(void)
+{
+    // this section is neccesary
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+    // second timer
+    ClockTime.second++;
+    if(ClockTime.second >= 60){
+        ClockTime.second = 0;
+        ClockTime.minute++;
+    }
+    if(ClockTime.minute >= 60){
+        ClockTime.minute = 0;
+        ClockTime.hour++;
+    }
+    if(ClockTime.hour >= 24){
+        ClockTime.hour = 0;
+        syncTime();
+    }
+
+    DrawTime();
+    DrawDate();
+}
