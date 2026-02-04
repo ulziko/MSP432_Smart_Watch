733c3039a50e63d0e0936a9a0bc75557afff0461
Raffaele-cell <raffaele.cella@studenti.unitn.it>
2026-02-04 12:24:22 +0100

    Fixed adc for accelerometer conversion

--- main.c
+++ main.c
@@ -63,9 +63,16 @@ void initGraphics(Graphics_Context *g_sContext) {
 
 void initADC() {
 
-    /* Configures Pin 6.0 and 4.4 as ADC input */
+    /* Configure the analog stick */
     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN0, GPIO_TERTIARY_MODULE_FUNCTION);
     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN4, GPIO_TERTIARY_MODULE_FUNCTION);
+
+    /* Configure the accelerometer */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN1, GPIO_TERTIARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN0, GPIO_TERTIARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN2, GPIO_TERTIARY_MODULE_FUNCTION);
+
+
 
     /* Initializing ADC (ADCOSC/64/8) */
     MAP_ADC14_enableModule();
@@ -73,7 +80,7 @@ void initADC() {
 
     /* Configuring ADC Memory (ADC_MEM0 - ADC_MEM1 (A15, A9)  with repeat)
          * with internal 2.5v reference */
-    MAP_ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM1, true);
+    MAP_ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM4, true);
     MAP_ADC14_configureConversionMemory(ADC_MEM0,
             ADC_VREFPOS_AVCC_VREFNEG_VSS,
             ADC_INPUT_A15, ADC_NONDIFFERENTIAL_INPUTS);
@@ -81,6 +88,18 @@ void initADC() {
     MAP_ADC14_configureConversionMemory(ADC_MEM1,
             ADC_VREFPOS_AVCC_VREFNEG_VSS,
             ADC_INPUT_A9, ADC_NONDIFFERENTIAL_INPUTS);
+
+    MAP_ADC14_configureConversionMemory(ADC_MEM2,
+            ADC_VREFPOS_AVCC_VREFNEG_VSS,
+            ADC_INPUT_A14, ADC_NONDIFFERENTIAL_INPUTS);
+
+    MAP_ADC14_configureConversionMemory(ADC_MEM3,
+            ADC_VREFPOS_AVCC_VREFNEG_VSS,
+            ADC_INPUT_A13, ADC_NONDIFFERENTIAL_INPUTS);
+
+    MAP_ADC14_configureConversionMemory(ADC_MEM4,
+            ADC_VREFPOS_AVCC_VREFNEG_VSS,
+            ADC_INPUT_A11, ADC_NONDIFFERENTIAL_INPUTS);
 
     /* Enabling the interrupt when a conversion on channel 1 (end of sequence)
      *  is complete and enabling conversions */
@@ -210,11 +229,18 @@ void ADC14_IRQHandler(void)
     status = MAP_ADC14_getEnabledInterruptStatus();
     MAP_ADC14_clearInterruptFlag(status);
 
-    uint32_t i;
-    for (i = 0; i < DIM(conversion_values); i ++)
-        conversion_values[i] = ADC14_getResult(1 << i);
+    if (current_task == STEP_COUNTER) {
+        conversion_values[0] = ADC14_getResult(ADC_MEM0);
+        conversion_values[1] = ADC14_getResult(ADC_MEM1);
 
-    handlers[current_task].adc_handler(status, conversion_values);
+        handlers[current_task].adc_handler(status, conversion_values);
+    }
+
+    conversion_values[0] = ADC14_getResult(ADC_MEM2);
+    conversion_values[1] = ADC14_getResult(ADC_MEM3);
+    conversion_values[2] = ADC14_getResult(ADC_MEM4);
+
+    handlers[STEP_COUNTER].adc_handler(status, conversion_values);
 }
 
 
