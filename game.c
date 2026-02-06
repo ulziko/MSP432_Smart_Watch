3deac47d6f0d84f6f0290caa3f8a8fabf3f420d6
Ulziko <ulziihishig247@gmail.com>
2026-02-06 17:07:12 +0100

    GAME SECTION: clearing screen handled, modified States

--- game.c
+++ game.c
@@ -6,72 +6,43 @@
 #include "game.h"
 #include <stdio.h>
 
-
-// toggle value for updating purpose
-volatile bool redraw = false;
-volatile bool init = false;
-volatile bool pipe_active = false;
-
-
-//constants
-#define GRAVITY        1
-#define FLAP_FORCE     5
-#define PIPE_SPEED     1
-#define SCREEN_HEIGHT  128
-#define SCREEN_WIDTH  128
-#define GROUND_Y       0
-#define PIPE_RANDOMNESS 4
-#define GAP_BETWEEN_PIPES 60
-
-// game score
-int score = 0;
-char score_string[10];
-
-// Pipe shifter value array
-volatile int pipe_index;
-int shifter[PIPE_RANDOMNESS]={-10, -50, -20, -40};
-
-
-//states
-GameState current_state= STATE_RESTART;
-
-
-////image declerations
-extern const Graphics_Image flappy_bird4BPP_UNCOMP[]; // 34x25 px
-// game over image
-extern const Graphics_Image  game_over4BPP_UNCOMP[]; //
-// upper road
-extern const Graphics_Image down_road4BPP_UNCOMP[]; //20x69 px
-// road down
-extern const Graphics_Image up_road4BPP_UNCOMP[]; //20x64 px
-// start button
-extern const Graphics_Image  start_button4BPP_UNCOMP[];
-
-
-
-
-
-typedef struct {
-    int x;
-    int y;
-    int old_x;
-    int old_y;
-    int w;
-    int h;
-} Sprite;
-
+// Sprite struct to represent the bird and pipes
 Sprite flappy;
 Sprite pipe1;
 Sprite pipe2;
 
+// toggle value for updating purpose
+volatile bool game_redraw;
+volatile bool pipe_active;
+volatile int score;
+volatile int pipe_index;
+volatile bool draw_screen_start_flag;
+
+static Graphics_Context *ctx_game = NULL;
+
+// game score
+char score_string[10];
+const int shifter[PIPE_RANDOMNESS] = {-10, -50, -20, -40};
+
+// Game initial state
+GameState game_current_state= STATE_INIT;
 
 
-
-
+/**
+ * @brief  Initialize game variables  and  sprite positions.
+ *
+ * @param  pContext  Graphics context pointer.
+ *
+ * @return Does not return a value.
+ *
+ * @note  This function sets the initial positions and dimensions of the flappy bird
+ *        and pipes, as well as initializes game state variables such as score and
+ *        pipe activity status.
+ */
 void Init(Graphics_Context *pContext)
-{
-    Graphics_setForegroundColor(pContext, GRAPHICS_COLOR_AQUA);
+{   
     Graphics_setBackgroundColor(pContext, GRAPHICS_COLOR_AQUA);
+    Graphics_clearDisplay(pContext);
     //Init flappy
     flappy.x = 47;
     flappy.y = 52;
@@ -98,8 +69,7 @@ void Init(Graphics_Context *pContext)
 
     // pipe calculation
     pipe_active=true;
-    redraw = false;
-    init = false;
+    game_redraw = false;
     pipe_index=0;
 
     // score restart
@@ -128,15 +98,14 @@ void redraw_game(Graphics_Context *pContext){
     GrImageDraw(pContext, up_road4BPP_UNCOMP, pipe1.x, pipe1.y);
     // draw lower road 1
     GrImageDraw(pContext, down_road4BPP_UNCOMP, pipe2.x, pipe2.y);
-
-
 }
 
 
 void draw_screen_start(Graphics_Context *pContext){
+    Graphics_setBackgroundColor(pContext, GRAPHICS_COLOR_AQUA);
     Graphics_clearDisplay(pContext);
     // flappy bird at the top
-    GrImageDraw(pContext, flappy_bird4BPP_UNCOMP, flappy.x, flappy.y);
+    GrImageDraw(pContext, flappy_bird4BPP_UNCOMP, 47, 52);
     // starting button of entering section
     GrImageDraw(pContext, start_button4BPP_UNCOMP, 39 , 80);
 }
@@ -156,47 +125,63 @@ void draw_screen_game_over(Graphics_Context *pContext){
 
 void game_task(Graphics_Context *pContext)
 {
-    switch (current_state){
-    case STATE_GAME:
-        if(init){
-            current_state= STATE_RESTART;
-            Graphics_clearDisplay(pContext);
-            init=false;
+    switch (game_current_state){
+
+    case STATE_START:
+        if (draw_screen_start_flag){
+            // draw the start screen
+            draw_screen_start(pContext);
+            // toggle the flag to avoid redrawing
+            draw_screen_start_flag=false;
+            // wake on button interrupt
+            PCM_gotoLPM0();
         }
-        if (redraw){
+        break;
+    case STATE_INIT:
+        Init(pContext);
+        game_current_state=STATE_PLAYING;
+        break;
+    
+    case STATE_PLAYING:
+        if (game_redraw){
+            // Flapp bird will fall down by gravity.
+            flappy.old_y=flappy.y;
+            flappy.y+=GRAVITY;
+            // Pipes will move to the left by pipe speed.
             pipe1.x-=PIPE_SPEED;
             pipe2.x-=PIPE_SPEED;
+            // redraw the game 
             redraw_game(pContext);
-            redraw=false;
+            game_redraw=false;
             if (flappy.y <= 0 || flappy.y + flappy.h >= SCREEN_HEIGHT) {
                 draw_screen_game_over(pContext);
-                current_state = STATE_GAME_OVER;
+                game_current_state = STATE_GAME_OVER;
+                PCM_gotoLPM0();
             }
-            // flappy is within pipe area
+            // X axis: flappy is within pipe area
             if( flappy.x+flappy.w > pipe1.x && flappy.x <pipe1.x+pipe1.w){
                 // does flappy.low is lower than  pipe2's lower part.
                 //and flappy's upper y is greater than pipe1's lower part which indicates intersection
                 if ((flappy.y+ flappy.h) >pipe2.y || flappy.y<pipe1.y+pipe1.h){
                     draw_screen_game_over(pContext);
-                    current_state=STATE_GAME_OVER;
+                    game_current_state=STATE_GAME_OVER;
+                    PCM_gotoLPM0();
                 }
             }
-            //
+            // flappy has passed the pipe
             if (flappy.x > (pipe1.x+ pipe1.w) && pipe_active){
                 score+=1;
                 pipe_active=false;
             }
+            // if the pipe is out of the screen, restart the pipe
             if (pipe1.x<0){
                 Graphics_clearDisplay(pContext);
                 restart_pipes();
             }
         }
         break;
-    case STATE_RESTART:
-        Init(pContext);
-        draw_screen_start(pContext);
-        current_state=STATE_START;
-        break;
+
+    // ignores other states
     default:
         break;
     }
@@ -206,24 +191,26 @@ void game_task(Graphics_Context *pContext)
 
 void game_ta0_handler(void)
 {
-    if (current_state == STATE_GAME) {
-        redraw = true;
-        flappy.old_y=flappy.y;
-        flappy.y+=GRAVITY;
+    // if the game is in playing state, update the position of the bird and set the redraw flag
+    if (game_current_state == STATE_PLAYING) {
+        game_redraw = true;
     }
 }
 
 
 void game_button1_handler()
 {
-    switch (current_state){
+    switch (game_current_state){
+    // button 1 is used to start the game when the game is in the start screen
     case STATE_START:
-        current_state=STATE_GAME;
+        game_current_state=STATE_INIT;
         break;
-    case STATE_GAME:
+    // button 1 is used to make the bird flap when the game is playing
+    case STATE_PLAYING:
        flappy.old_y=flappy.y;
        flappy.y-=FLAP_FORCE;
         break;
+    // ignores other states
     default:
         break;
     }
@@ -232,14 +219,17 @@ void game_button1_handler()
 
 void game_button2_handler()
 {
-    switch (current_state){
-          case STATE_GAME_OVER:
-              current_state=STATE_RESTART;
-              break;
-          default:
-              break;
+    if (game_current_state==STATE_GAME_OVER){
+        // button 2 is used to restart the game when the game is over
+        draw_screen_start_flag=true;
+        game_current_state=STATE_START;
     }
 }
 
 
 
+
+void game_exit_handler(){
+    draw_screen_start_flag=true;
+    game_current_state=STATE_START;
+}
