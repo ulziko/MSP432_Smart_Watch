7b674bd47dbc96408ebb1bdb0da11d7128322e42
Raffaele-cell <raffaele.cella@studenti.unitn.it>
2026-02-06 12:00:53 +0100

    Added exit handler

--- tasks.c
+++ tasks.c
@@ -13,11 +13,11 @@
 tasks_t current_task = TIME_DISPLAY;
 
 const t_task handlers[] = {
-     {main_page_task, main_page_adc, no_op_task, no_op_task, main_page_button, main_page_button, "Main Page", MAIN_PAGE},
-     {time_display, no_op_adc_task, no_op_task, time_display_ta1_handler , no_op_task, no_op_task, "Time Display", TIME_DISPLAY},
-     {alarm_task, alarm_adc, no_op_task,  no_op_task, alarm_button1_handler, alarm_button2_handler, "Alarm", ALARM},
-     {game_task, no_op_adc_task, game_ta0_handler,  no_op_task, game_button1_handler, game_button2_handler, "Game", GAME},
-     {step_counter_task, activity_tracker_adc_handler, no_op_task, activity_tracker_timer_tick, no_op_task, no_op_task, "Steps", STEP_COUNTER},
+     {main_page_task, main_page_adc, no_op_task, no_op_task, main_page_button, main_page_button, no_op_task, "Main Page", MAIN_PAGE},
+     {time_display, no_op_adc_task, no_op_task, time_display_ta1_handler , no_op_task, no_op_task, no_op_task, "Time Display", TIME_DISPLAY},
+     {alarm_task, alarm_adc, no_op_task,  no_op_task, alarm_button1_handler, alarm_button2_handler, no_op_task, "Alarm", ALARM},
+     {game_task, no_op_adc_task, game_ta0_handler,  no_op_task, game_button1_handler, game_button2_handler, no_op_task, "Game", GAME},
+     {step_counter_task, activity_tracker_adc_handler, no_op_task, activity_tracker_timer_tick, no_op_task, no_op_task, no_op_task, "Steps", STEP_COUNTER},
 };
 
 void no_op_adc_task(uint64_t status, uint16_t* conversionValues) {
